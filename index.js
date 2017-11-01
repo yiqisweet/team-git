8bd824c6991fa06efc2b9cb0dd98838e93222267
yiqisweet <mynameislxmax@outlook.com>
2017-11-01 14:46:02 +0800

    我是李鑫，我提交了一个功能

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
  * @Date:   2017-11-01 02:29:09
  * @Email:  mynameislxmax@outlook.com
  * @Last modified by:   randy
- * @Last modified time: 2017-11-01 02:33:10
+ * @Last modified time: 2017-11-01 02:44:24
  */
-console.log('李鑫');
+ console.log('李鑫鑫')
