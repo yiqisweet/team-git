039bed49365a534314c8631d44c0aa37be6f9dcf
tangfanny <tangfanny@sina.cn>
2017-11-01 14:51:16 +0800

    我是唐丽丽，我提交了一个功能

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  * @Date:   2017-11-01 02:29:09
  * @Email:  mynameislxmax@outlook.com
  * @Last modified by:   randy
- * @Last modified time: 2017-11-01 02:44:24
+ * @Last modified time: 2017-11-01 02:50:52
  */
  console.log('李鑫鑫')
+console.log('唐丽丽')
